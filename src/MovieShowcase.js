e46c475901a29a3c780db045bea7152050da39ca
Tim Koar <koartimothy@gmail.com>
2019-06-24 21:19:48 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
+import React, { Component } from 'react'; // This gives us access to all the methods available to us in react
+import MovieCard from './card-components/MovieCard.js' // this gives us access to the movieCards
+// we want to use to display information about each movie
 import movieData from './data.js'
+// this allows you to have access to the hash of movies in the
+// data file './data.js'
 
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+
+    return movieData.map((movie, i) => <MovieCard key={i} title={movie.title} genres={movie.genres} poster={movie.poster} IMDBRating={movie.IMDBRating}/>);
   }
 
   render() {
